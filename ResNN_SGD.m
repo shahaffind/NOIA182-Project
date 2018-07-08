92acfb454338b0d7c930b2e9f67548aa78cef03b
Shahaf Finder <shahaffind@gmail.com>
2018-07-09 02:45:54 +0300

    playing with sgd

--- ResNN_SGD.m
+++ ResNN_SGD.m
@@ -1,12 +1,18 @@
-function [ Theta_k, records, i ] = ResNN_SGD( X, C, Theta, n_layers, batch_size, max_epoch, Yv, Cv, record_every )
+function [ Theta_k, records ] = ResNN_SGD( X, C, Theta, n_layers, batch_size, max_epoch, Yv, Cv, record_every )
     
     [~, n_samples] = size(X);
     
+    s = RandStream('mt19937ar', 'Seed', 2567);
+    
+    base_alpha = 1 / 100;
+    base_gamma = 0.7;
+    
     records = zeros(ceil(max_epoch / record_every), 2);
     Theta_k = Theta;
-    
+    m_k = zeros(size(Theta));
+    tic;
     for i=1:max_epoch
-        idxs = randperm(n_samples);
+        idxs = randperm(s, n_samples);
         for k = 1:(n_samples / batch_size)
             idx_k = idxs((k-1) * batch_size + 1 : k * batch_size);
             X_k = X(:,idx_k);
@@ -16,20 +22,31 @@ function [ Theta_k, records, i ] = ResNN_SGD( X, C, Theta, n_layers, batch_size,
             g_k = back_propagation(Xis, C_k, Theta_k, n_layers);
             
             if i <= 100
-                a_k = 1 / 100;
+                a_k = base_alpha;
+                gamma = base_gamma;
             else
-                a_k = 1 / (10*sqrt(i));
+                a_k = base_alpha * 5 / sqrt(i);
+                gamma = base_gamma * 10 / sqrt(i);
             end
-            Theta_k = Theta_k - a_k * g_k;
+            
+            m_proj = g_k * m_k' * g_k / (norm(g_k)*norm(g_k));
+            m_rej = m_k - m_proj;
+            %m_k = m_rej;
+            %gamma = 0;
+            
+            m_k = a_k * g_k + gamma * m_k;
+            Theta_k = Theta_k - m_k;
         end
         
         if mod(i, record_every) == 0
             [all_proba, loss_val, ~] = forward_pass(Yv, Cv, Theta_k, n_layers);
             [c_p, R] = correct_percent(all_proba, Cv);
             records(i / record_every, :) = [loss_val, c_p];
+            toc;
             
-            viewFeatures2D(Yv, R);
-            drawnow update
+%              viewFeatures2D(Yv, R);
+%              drawnow update
+            
             fprintf('iter:%d\n\tloss: %f\n\tcorrect: %f\n\n', i, loss_val, c_p);
         end
     end
--- run_net.m
+++ run_net.m
@@ -1,19 +1,23 @@
-load('./data/PeaksData.mat')
+load('./data/SwissRollData.mat')
 
 % parameters
 [n_labels, ~] = size(Ct);
 [dim, ~] = size(Yt);
-max_epoch = 10000;
-n_layers = 15;
+max_epoch = 500;
+n_layers = 3;
 batch_size = 50;
 
-record_every = 10;
+record_every = 1;
 
 theta_layer_size = dim + (dim^2) * 2;  % 2* NxN matrix + N vector
 loss_layer_size = n_labels * (dim + 1);  % NxL matrix + L vector
 
 % init all weights
-Theta = randn(n_layers * theta_layer_size + loss_layer_size, 1);
+%Theta = randn(n_layers * theta_layer_size + loss_layer_size, 1);
 
 % fit the data
-[Theta_new, Theta_all, iters] = ResNN_SGD(Yt, Ct, Theta, n_layers, batch_size, max_epoch, Yv, Cv, record_every);
+[Theta_new, records] = ResNN_SGD(Yt, Ct, Theta, n_layers, batch_size, max_epoch, Yv, Cv, record_every);
+
+figure(99);
+hold on
+plot(records(:,1));
